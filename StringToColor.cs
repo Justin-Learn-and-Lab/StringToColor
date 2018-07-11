38aebf47762af4efb6d67570af819ac10e40600a
Justin <imjyb1008work@gmail.com>
2018-07-11 21:17:15 +0800

    Update StringToColor.cs

--- StringToColor.cs
+++ StringToColor.cs
@@ -1,56 +1,58 @@
-public class StringToColor
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace StringToColor
 {
-    public Color Transfer(string name)
-    {
-        Color transferResult = null;
-
-        List<IColor> colors = new List<IColor>();
-		colors.Add(new Red());
-		colors.Add(new Green());
-		colors.Add(new Blue());
-
-		foreach(IColor item in colors)
+	public class StringToColor
+	{
+		public Color Transfer(string name)
 		{
-			if( item.GetType().Name.ToLower() == name.ToLower())
+			List<IColor> colors = new List<IColor>();
+			colors.Add(new Red());
+			colors.Add(new Green());
+			colors.Add(new Blue());
+
+			foreach (IColor item in colors)
 			{
-				transferResult = item.GetColor();
-				break;
+				if (item.GetType().Name.ToLower() == name.ToLower())
+				{
+					return item.GetColor();
+				}
 			}
-		}
-	
-		if(transferResult !=null)
-			return transferResult;
-		else
+
 			throw new ArgumentException("不正確的顏色名稱");
+		}
+	}
+
+	interface IColor
+	{
+		Color GetColor();
+	}
+
+	public class Red : IColor
+	{
+		public Color GetColor()
+		{
+			return Color.FromArgb(0xFF, 0xFF, 0x00, 0x00);
+		}
+	}
+
+	public class Green : IColor
+	{
+		public Color GetColor()
+		{
+			return Color.FromArgb(0xFF, 0x80, 0x80, 0x80);
+		}
+	}
+
+	public class Blue : IColor
+	{
+		public Color GetColor()
+		{
+			return Color.FromArgb(0xFF, 0x00, 0x00, 0xFF);
 		}
 	}
 }
 
-interface IColor
-{
-    Color GetColor();
-}
 
-public class Red : IColor
-{
-    public Color GetColor()
-    {
-		return Color.FromArgb(0xFF, 0xFF, 0x00, 0x00);
-    }
-}
-
-public class Green : IColor
-{
-	public Color GetColor()
-    {
-		return Color.FromArgb(0xFF, 0x80, 0x80, 0x80);
-    }
-}
-
-public class Blue : IColor
-{
-    public Color GetColor()
-    {
-		return Color.FromArgb(0xFF, 0x00, 0x00, 0xFF);
-    }
-}
